b336dc01df7845bb0b5c829793186265b556bf36
ilomedia <contact@ilomedia.net>
2019-11-29 16:40:39 +0100

    harmonisation code et gestion seuil confidence

--- action-rdv.py
+++ action-rdv.py
@@ -20,22 +20,26 @@ def read_configuration_file(configuration_file):
     try:
         with io.open(configuration_file, encoding=CONFIGURATION_ENCODING_FORMAT) as f:
             conf_parser = SnipsConfigParser()
-            conf_parser.readfp(f)
+            conf_parser.read_file(f)
             return conf_parser.to_dict()
     except (IOError, configparser.Error) as e:
         return dict()
 
 
-def subscribe_intent_callback(hermes, intent_message):
+def subscribe_intent_callback(hermes, intentMessage):
     conf = read_configuration_file(CONFIG_INI)
-    action_wrapper(hermes, intent_message, conf)
+
+    if intentMessage.asr_confidence < conf['global']['confidence_threshold']:
+        hermes.publish_end_session(intentMessage.session_id)
+    else:
+        action_wrapper(hermes, intentMessage, conf)
 
 
-def action_wrapper(hermes, intent_message, conf):
+def action_wrapper(hermes, intentMessage, conf):
 
     sentence = "Vous avez rendez-vous avec le podologue à 2 heure de l'après-midi"
 
-    hermes.publish_end_session(intent_message.session_id, sentence)
+    hermes.publish_end_session(intentMessage.session_id, sentence)
 
 
 if __name__ == "__main__":
